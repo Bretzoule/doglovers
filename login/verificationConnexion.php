09d8667c44972ad1fb38cb0a1c7b35a4c97b9a2f
Lefloch Thomas <leflochtho@eisti.eu>
2020-05-19 15:00:18 +0200

    ajout premiers jets fonction de connexion

--- login/verificationConnexion.php
+++ login/verificationConnexion.php
@@ -4,12 +4,32 @@ function phpAlert($msg) {
     echo '<script type="text/javascript">alert("' . $msg . '")</script>';
 }
 
-$file = fopen('./../users/users.txt', 'r'); // ouverture du fichier
+$file = fopen('./../register/userList.txt', 'r'); // ouverture du fichier
 if ($file) {
     while (($line = fgets($file)) !== false) {
-        $student = explode(";", $line);
-        if ((password_verify(trim($_POST["password"]),trim($student[sizeof($student)-1]))) && htmlspecialchars((trim($_POST["pseudo"])) == trim($student[sizeof($student)-2]))) {
-            
+        $userData = explode(";", $line);
+        if ((password_verify(trim($_POST["password"]),trim($userData[sizeof($userData)-1])) && (trim($_POST["pseudo"]) == trim($userData[sizeof($userData)-2])))) {
+            $_SESSION["udata"] = array();
+            $_SESSION["udata"] = array_slice($userData,0,sizeof($userData)-2);
+            header('Location: ./../home/accueil.php');
+            fclose($file);
+            exit();
+        }
+    }
+    fclose($file);
+} else {
+    phpAlert("Une erreur est survenue lors de l'accès au site...Veuillez réessayer!");
+}
+$file = fopen('./../register/adminList.txt', 'r'); // ouverture du fichier
+if ($file) {
+    while (($line = fgets($file)) !== false) {
+        $userData = explode(";", $line);
+        if ((password_verify(trim($_POST["password"]),trim($userData[sizeof($userData)-1])) && (trim($_POST["pseudo"]) == trim($userData[sizeof($userData)-2])))) {
+            $_SESSION["udata"] = array();
+            $_SESSION["udata"] = array_slice($userData,0,sizeof($userData)-2);
+            $_SESSION["admin"] = true;
+            header('Location: ./../home/accueil.php');
+            fclose($file);
             exit();
         }
     }
