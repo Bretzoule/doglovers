19641cdcbd94890e223a05efc761337bc92c0064
Thomas <leflochtho@eisti.eu>
2020-05-19 16:16:41 +0200

    ajout vérification de la portée de l'abonnement

--- login/verificationConnexion.php
+++ login/verificationConnexion.php
@@ -4,6 +4,19 @@ function phpAlert($msg) {
     echo '<script type="text/javascript">alert("' . $msg . '")</script>';
 }
 
+function getMemberShipType($memberShipString) {
+    $infoMS = explode(":",$memberShipString);
+    if ($infoMS[0] = "free") {
+        return "free";
+    } else {
+        if (date("Y-m-d") > $infoMS[1]) {
+            return "free";
+        } else {
+            return "member";
+        }
+    }
+}
+
 $file = fopen('./../register/userList.txt', 'r'); // ouverture du fichier
 if ($file) {
     while (($line = fgets($file)) !== false) {
@@ -11,6 +24,7 @@ if ($file) {
         if ((password_verify(trim($_POST["password"]),trim($userData[sizeof($userData)-1])) && (trim($_POST["pseudo"]) == trim($userData[sizeof($userData)-2])))) {
             $_SESSION["udata"] = array();
             $_SESSION["udata"] = array_slice($userData,0,sizeof($userData)-2);
+            $_SESSION["login_Type"] = getMemberShipType($_SESSION["udata"][sizeof($userData)-5]);
             header('Location: ./../home/accueil.php');
             fclose($file);
             exit();
@@ -27,7 +41,7 @@ if ($file) {
         if ((password_verify(trim($_POST["password"]),trim($userData[sizeof($userData)-1])) && (trim($_POST["pseudo"]) == trim($userData[sizeof($userData)-2])))) {
             $_SESSION["udata"] = array();
             $_SESSION["udata"] = array_slice($userData,0,sizeof($userData)-2);
-            $_SESSION["admin"] = true;
+            $_SESSION["login_Type"] = "admin";
             header('Location: ./../home/accueil.php');
             fclose($file);
             exit();
