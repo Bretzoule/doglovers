25cb92235e5294ae7f9f5e6175804b3a16c3f4cd
Thomas <leflochtho@eisti.eu>
2020-05-26 12:41:29 +0200

    ajout gestion d'erreurs

--- errors/erreur403.php
+++ errors/erreur403.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8">
-    <title>Dog Lovers - Erreur Authentification</title>
+    <title>Dog Lovers - Erreur 403</title>
     <link rel="stylesheet" type="text/css" href="./erreurs.css">
     <link rel="shortcut icon" href="./../ressources/favicon.ico" />
 </head>
@@ -12,18 +12,18 @@
     <div id="blocTitre"></div>
     <div id="Titre">
     <img src="/ressources/dogloverslogo.png" alt="logoDogLovers"> 
-        <h1>Erreur d'authentification</h1>
+        <h1>Erreur 403</h1>
     </div>
     <div class="menu">
         <ul>
-            <li><a href="./accueil.php">Accueil</a></li>
-            <li class="deconnexion"><a href="./../login/logout.php">Deconnexion</a></li>
+            <li><a href="/home/accueil.php">Accueil</a></li>
+            <li class="deconnexion"><a href="/login/logout.php">Deconnexion</a></li>
         </ul>
     </div>
     <div id="page">
-        <h2 id='titreTab'> Erreur d'Authentification</h2>
+        <h2 id='titreTab'> Erreur 403</h2>
         <div id="BlocInfo">
-            <span id="titreInfo">Il semblerait que vous soyez déjà connecté / que vous n'ayez pas accès à cette ressource.</span> <br>
+            <span id="titreInfo">Il semblerait que vous n'ayez pas accès à cette ressource.</span> <br>
         </div>
     </div>
 </body>
