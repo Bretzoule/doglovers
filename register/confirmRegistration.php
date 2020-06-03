a0a4e50c2bfb437a7a316da5027b3d3d069d5ebf
Thomas <leflochtho@eisti.eu>
2020-06-03 15:50:54 +0200

    debuggage fonction vérification enregistrement

--- register/confirmRegistration.php
+++ register/confirmRegistration.php
@@ -1,57 +1,68 @@
 <!-- Enregistrement des inscirts -->
 <?php
 session_start();
+function phpAlert($msg)
+{
+  echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+}
 
-function checkAlreadyRegistered() {
-  $file = fopen('./../register/data/userList.txt', 'r');
+function checkAlreadyRegistered()
+{
+  $lastvalue = true;
+  $file = fopen('./data/userList.txt', 'r');
   if ($file) {
-    while (($line = fgets($file)) !== false) {
-        $userData = explode(";", $line);
-        if ((trim($_SESSION["pseudo"]) == trim($userData[sizeof($userData)-2]))) {
-            $_SESSION["erreur"] = "login_existant";
-            header('Location: register/register.php');
-            fclose($file);
-            exit();
-        }
+    while ((($line = fgets($file)) !== false) && $lastvalue) {
+      $userData = explode("§", $line);
+      echo trim("|" . $_SESSION["pseudo"]) . "| == |" . trim($userData[sizeof($userData) - 2]) . "|";
+      if ((trim($_SESSION["pseudo"]) == trim($userData[sizeof($userData) - 2])) || (trim($_SESSION["adresse"]) == trim($userData[1]))) {
+        $_SESSION["erreur"] = "login_existant";
+        $lastvalue = false;
+      }
     }
     fclose($file);
-} else {
+    return($lastvalue);
+  } else {
     phpAlert("Une erreur est survenue lors de l'accès au site...Veuillez réessayer!");
+  }
 }
-}
+
 //on démarre la session
-if (($_SESSION["dataPassed"] = "true") && (!checkAlreadyRegistered())) {
-  
-  if ($_SESSION['enfants'] == "on") {
-    $nbrEnfants = $_SESSION['nombreEnf'];
-  } else {
-    $nbrEnfants = "0";
+
+if (isset($_SESSION["dataPassed"]) && ($_SESSION["dataPassed"] == "true")) {
+  if (checkAlreadyRegistered()) {
+    if ($_SESSION['enfants'] == "on") {
+      $nbrEnfants = $_SESSION['nombreEnf'];
+    } else {
+      $nbrEnfants = "0";
+    }
+    if ($_SESSION['chiens'] == "on") {
+      $nbrDoggos = $_SESSION['nbDoggos'];
+      $infosChiens = $_SESSION['infoschiens'];
+    } else {
+      $nbrDoggos = "0";
+      $infosChiens = "";
+    }
+    //on récupère les données de l'inscrit dans une variable
+    $content = $_SESSION['nom'] . "|" . $_SESSION['prenom'] .
+      "§" . $_SESSION['adresse'] . "|" . $_SESSION['lieures']
+      . "§" . $_SESSION['sexe'] . "§" . $_SESSION['dateNaissance'] . "§" . $_SESSION['profession']
+      . "§" . $_SESSION['situation'] . "|" . $nbrEnfants
+      . "§" . $_SESSION['poids'] . "|" . $_SESSION['taille'] . "|" . $_SESSION['couleurCheveux'] . "|" . $_SESSION['couleurYeux']
+      . "§" . $_SESSION['msgAcc'] . "§" . $_SESSION['citation'] . "§" . $_SESSION['interets'] . "§" . $_SESSION['fumeur']
+      . "§" . $nbrDoggos . "|" . $infosChiens
+      . "§" . $_SESSION['photos']
+      . "§" . "free" . "§" . date("Y-m-d") . "§" . uniqid($prefix = "user_")
+      . "§" . $_SESSION['pseudo'] . "§" . password_hash($_SESSION['password'], PASSWORD_DEFAULT) . "\r\n";
+    //on écrit ce que contient la variable dans le fichier nommé userList.txt
+    //FILE_APPEND permet d'écrire à la suite du fichier
+    file_put_contents('./data/userList.txt', $content, FILE_APPEND);
+    // remove all session variables
+    session_unset();
+    session_destroy();
+    session_start();
+    $_SESSION["inscrit"] = "success";
+    header('location: /login/login.php');
   }
-  if ($_SESSION['chiens'] == "on") {
-    $nbrDoggos = $_SESSION['nbDoggos'];
-    $infosChien = $_SESSION['infoschiens'];
-  } else {
-    $nbrDoggos = "0";
-    $infosChien = "";
-  }
-  //on récupère les données de l'inscrit dans une variable
-  $content = $_SESSION['nom'] . "|" . $_SESSION['prenom'] .
-    "§" . $_SESSION['adresse'] . "|" . $_SESSION['lieures']
-    . "§" . $_SESSION['sexe'] . "§" . $_SESSION['dateNaissance'] . "§" . $_SESSION['profession']
-    . "§" . $_SESSION['situation'] . "|" . $nbrEnfants
-    . "§" . $_SESSION['poids'] . "|" . $_SESSION['taille'] . "|" . $_SESSION['couleurCheveux'] . "|" . $_SESSION['couleurYeux']
-    . "§" . $_SESSION['msgAcc'] . "§" . $_SESSION['citation'] . "§" . $_SESSION['interets'] . "§" . $_SESSION['fumeurs']
-    . "§" . $nbrDoggos . "|" . $infosChiens
-    . "§" . $_SESSION['photo'] 
-    . "§" . "free" . "§" . date("Y-m-d") . "§" . uniqid($prefix = "user_")
-    . $_SESSION['pseudo'] . "§" . password_hash($_SESSION['password'],PASSWORD_DEFAULT) . " \r\n";
-  //on écrit ce que contient la variable dans le fichier nommé userList.txt
-  //FILE_APPEND permet d'écrire à la suite du fichier
-  file_put_contents('userList.txt', $content, FILE_APPEND);
-  // remove all session variables
-  session_unset();
-  session_destroy();
-  header('location: /login/login.php');
 } else {
   header('Location: /erreurs/forbidden.php');
 }
