71a7300e26f7e7783120df37382ccecb79adc16a
Thomas <leflochtho@eisti.eu>
2020-06-05 14:04:29 +0200

    creation page de réinitialisation de mot de passe

---
+++ login/resetPassword.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html "-//W3C//DTD XHTML 1.0 Strict //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1−strict.dtd">
+<html>
+
+<head>
+
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+  <title>Dog Lovers - Le site de rencontre pour les amoureux des chiens.</title>
+  <link rel="stylesheet" type="text/css" href="./login.css">
+  <link rel="shortcut icon" href="./../ressources/favicon.ico"/>
+</head>
+
+<body>
+  <div id="bloc_colonne">
+    <div class="Colonne">
+    <div id="bloc_Image">
+      <img src="/ressources/dogloverslogo.png" alt="logo"></img>
+    </div>
+  </div>
+  <div id="bloc_placement">
+</div>
+<!--
+  <div class="Colonne"><img src="/ressources/dogloverslogo.png" alt="">Nom 1</img></div>
+<div class="Colonne"><img src="/ressources/dogloverslogo.png" alt="">Nom 2</img></div>
+<div class="Colonne"><img src="/ressources/dogloverslogo.png" alt="">Nom 3</img></div>
+<div class="Colonne"><img src="/ressources/dogloverslogo.png" alt="">Nom 4</img></div>
+-->
+</div>
+</div>
+</body>
+</html>
