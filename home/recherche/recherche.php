bad060ea1747d908173456817f3f1c73b9ad4fac
Thomas <leflochtho@eisti.eu>
2020-06-07 16:00:35 +0200

    amélioration recherche et prise en charge des multi-éléments

--- home/recherche/recherche.php
+++ home/recherche/recherche.php
@@ -1,5 +1,25 @@
 <?php
 
+function checkFumeur(string $field) : string
+{   
+    $tmp = "";
+    if ($field == "") {
+        $tmp = "Non fumeur";
+    }
+    return $tmp;
+}
+
+function getDateNaissance(string $date) : string
+{
+    $dateAnniv = explode("-", $date);
+    $dateAjd = explode("-", date("Y-m-d"));
+      $age = intval($dateAjd[0]) - intval($dateAnniv[0]);
+      if ((intval($dateAjd[1]) < intval($dateAnniv[1]) || ((intval($dateAjd[1])) == intval($dateAnniv[1])) && (intval($dateAjd[2]) < intval($dateAnniv[2])))) {
+        $age--;
+      }
+    return($age . "ans");
+}
+
 $response = "";
 echo $response;
 $elementsRecherche = explode(' ', $_GET["recherche"]); // recupération des mots clés.
@@ -8,7 +28,9 @@ $data = array();
 if ($file) {
     while (($line = fgets($file)) !== false) {
         $userData = explode("§", $line);
-        array_push($data,array_slice($userData,0,sizeof($userData)-7));
+        $userData[10] = checkFumeur($userData[10]); // permet d'écrire si l'utilisateur fume ou non
+        $userData[3] = getDateNaissance($userData[3]); // permet d'écrire l'age de l'utilisateur 
+        array_push($data,array_slice($userData,0,sizeof($userData)-7)); // ajoute l'utilisateur à la liste de recherche
     }
     fclose($file);
 } else {
@@ -19,9 +41,9 @@ foreach($data as $utilisateur) {
     foreach ($elementsRecherche as $keyword) {
         $found = false;
         $i = 0;
-        while ($i < sizeof($utilisateur) && !$found) {
-            if (stristr($utilisateur[$i],$keyword)) {
-                 $response .= $utilisateur[0];
+        while ($i < sizeof($utilisateur) && !$found && (strpos($response,$utilisateur[0]) === false )) { // permet de vérifier tout les données de l'utilisateur
+            if (stristr($utilisateur[$i],$keyword)) { // , tant que rien de cohérent n'a été trouvé et que l'utilisateur ne matche pas déjà avec un keyword
+                 $response .= $utilisateur[0]; // ajoute l'utilisateur aux résultats
                 $found = true;
             }
             $i++;
