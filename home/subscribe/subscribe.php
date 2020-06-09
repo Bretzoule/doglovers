03ec4bbf4545eb428dba1a01dac8c8a7cfd296a9
Thomas <leflochtho@eisti.eu>
2020-06-09 22:39:46 +0200

    ajout fonctionnalité de récupération du type d'abonnement et de sa date d'expiration

--- home/subscribe/subscribe.php
+++ home/subscribe/subscribe.php
@@ -5,14 +5,29 @@ if ((isset($_SESSION["login_Type"])) && (intval($_SESSION["login_Type"]) > 0)) {
   <html>
     <?php 
     function getDateFinAbonnement(string $username):string {
-      return "hello world";
+      $path = "./../../register/data/userList.txt";
+      $file = fopen($path, 'r');
+      if ($file) {
+          $lastvalue = true;
+          while ((($line = fgets($file)) !== false) && $lastvalue) {
+              $userData = explode("§", $line);
+              if ((trim($_SESSION["Pseudo"]) == trim($userData[0]))) {
+                $tmpdate = explode(':',trim($userData[sizeof($userData)-6]));
+                $date = $tmpdate[1];
+                $lastvalue = false;
+              }
+          }
+          fclose($file);
+      } else {
+          phpAlert("Une erreur est survenue lors de l'accès au site...Veuillez réessayer!");
+      }
+      return $date;
     }
     ?>
   <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8">
     <title>Dog Lovers - Accueil</title>
     <link rel="stylesheet" type="text/css" href="./subscribe.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="shortcut icon" href="./../ressources/favicon.ico" />
   </head>
 
@@ -26,16 +41,16 @@ if ((isset($_SESSION["login_Type"])) && (intval($_SESSION["login_Type"]) > 0)) {
       <ul>
         <li><a class="active" href="./accueil.php">Accueil</a></li>
         <li><a class="active" href="../profil/MonProfil.php">Mon profil</a></li>
-        <li class="deconnexion"><a href="./../login/logout.php">Deconnexion</a></li>
+        <li class="deconnexion"><a href="./../../login/logout.php">Deconnexion</a></li>
       </ul>
     </div>
     <div id="page">
       <h2 id='titreTab'> Formules d'abonnement</h2>
       <div id="BlocInfo">
         <span id="titreInfo"> Gratuit</span> <br>
-        <span> <?php if ($_SESSION["login_type"] == 1) { echo "votre type d'abonnement en cours."; }?> </span> <br>
+        <span> <?php if ($_SESSION["login_Type"] == 1) { echo "votre type d'abonnement en cours."; }?> </span> <br>
         <span id="titreInfo"> Abonné </span> <br>
-        <span> <?php if ($_SESSION["login_type"] == 2) { echo "votre type d'abonnement en cours. <br>"; 
+        <span> <?php if ($_SESSION["login_Type"] == 2) { echo "votre type d'abonnement en cours. <br>"; 
           echo "jusqu\'au" . getDateFinAbonnement($_SESSION["pseudo"]);
         }?> </span> <br>
       </div>
