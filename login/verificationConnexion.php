0bdb6b2a87c8c379e5ac0a23d35fbf1a7bc751e7
Thomas <leflochtho@eisti.eu>
2020-06-09 21:24:57 +0200

    ajout traitement administrateur en commentaire

--- login/verificationConnexion.php
+++ login/verificationConnexion.php
@@ -35,22 +35,22 @@ if ($file) {
 } else {
     phpAlert("Une erreur est survenue lors de l'accès au site...Veuillez réessayer!");
 }
-$file = fopen('./../register/data/adminList.txt', 'r'); // ouverture du fichier
-if ($file) {
-    while (($line = fgets($file)) !== false) {
-        $userData = explode("§", $line);
-        if ((password_verify(trim($_POST["password"]),trim($userData[sizeof($userData)-1])) && (trim($_POST["pseudo"]) == trim($userData[0])))) {
-            $_SESSION["udata"] = array();
-            $_SESSION["udata"] = array_slice($userData,0,sizeof($userData)-2);
-            $_SESSION["login_Type"] = "3";
-            header('Location: ./../home/accueil.php');
-            fclose($file);
-            exit();
-        }
-    }
-    fclose($file);
-} else {
-    phpAlert("Une erreur est survenue lors de l'accès au site...Veuillez réessayer!");
-}
+// $file = fopen('./../register/data/adminList.txt', 'r'); // ouverture du fichier pour le administrateurs
+// if ($file) {
+//     while (($line = fgets($file)) !== false) {
+//         $userData = explode("§", $line);
+//         if ((password_verify(trim($_POST["password"]),trim($userData[sizeof($userData)-1])) && (trim($_POST["pseudo"]) == trim($userData[0])))) {
+//             $_SESSION["udata"] = array();
+//             $_SESSION["udata"] = array_slice($userData,0,sizeof($userData)-2);
+//             $_SESSION["login_Type"] = "3";
+//             header('Location: ./../home/accueil.php');
+//             fclose($file);
+//             exit();
+//         }
+//     }
+//     fclose($file);
+// } else {
+//     phpAlert("Une erreur est survenue lors de l'accès au site...Veuillez réessayer!");
+// }
     $_SESSION["error"] = "error";
     header('Location: /login/login.php');
