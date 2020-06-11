cb1da4a1a379934d454c41adf9223e373eaa6439
Thomas <leflochtho@eisti.eu>
2020-06-11 15:58:59 +0200

    ajout fonction de prolongement d'abonnement

--- home/subscribe/confirmSubscription.php
+++ home/subscribe/confirmSubscription.php
@@ -1,4 +1,39 @@
 <?php 
+    session_start();
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    function subAdd(string $toAdd, string $memberType):string
+    {
+        $tmp = explode(':',$memberType);
+        $currentDate = (empty($tmp[1])) ? new DateTime(date("Y-m-d")) : new DateTime($tmp[1]); // génération de la date
+        $currentDate -> add(new DateInterval($toAdd));
+        $tmp[0] = "member";
+        $tmp[1] = $currentDate->format('Y-m-d');
+        return(implode(":", $tmp));
+    }
+
+    function addSubscription(string $toAdd)
+    {
+            $path = "./../../register/data/userList.txt"; // chemin fichier utilisateur
+            $file = fopen($path, 'r'); // ouverture du fichier
+            if ($file) { // si le fichier est bien ouvert alors
+                $lastvalue = true; 
+                while ((($line = fgets($file)) !== false) && $lastvalue) { // on récupère chaque ligne tant que l'on trouve pas l'utilisateur
+                    $userData = explode("§", $line); // séparation des données de la ligne utilisateur
+                    //echo "|" . trim($_SESSION["adresseM"]) . "| == |" . trim($userData[sizeof($userData)-2]) . "| <br>";
+                    if ((trim($_SESSION["Pseudo"]) == trim($userData[0]))) { // si le pseudo correspond a un pseudo  en bdd alors 
+                        $contents = file_get_contents($path); // récupération des données du fichier 
+                        $userData[sizeof($userData)-6] = subAdd($toAdd,$userData[sizeof($userData)-6]); // mise à jour de l'abonnement
+                        $contents = str_replace($line,$userData . "\r\n",$contents);  // ajout de la ligne dans les données
+                        file_put_contents($path, $contents); // ajout des données au fichier
+                        $lastvalue = false;
+                    }
+                }
+                fclose($file);
+            } else {
+                phpAlert("Une erreur est survenue lors de l'accès au site...Veuillez réessayer!");
+            }
+    }
+
     $typeAbo = $_GET["abonnement"];
     switch ($typeAbo) {
         case '48h':
@@ -22,4 +57,7 @@
             break;
     }
     header("Location: ./subscribe.php");
+} else {
+    header("Location: /home/accueil.php");
+}
 ?>
