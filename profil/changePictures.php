7169bea054e7161987b82cd86c9cd8b774532724
Thomas <leflochtho@eisti.eu>
2020-06-15 01:59:06 +0200

    ajout boutton de retour au menu (image)

--- profil/changePictures.php
+++ profil/changePictures.php
@@ -8,18 +8,16 @@ if ((isset($_SESSION["login_Type"])) && (intval($_SESSION["login_Type"]) > 0)) {
         <meta http-equiv="content-type" content="text/html;charset=UTF-8">
         <title>Dog Lovers - Recherche</title>
         <link rel="stylesheet" type="text/css" href="./modificationPw.css">
-        <script src="./recherche.js"></script>
         <link rel="shortcut icon" href="./../ressources/favicon.ico" />
     </head>
-
     <body>
     <div id="bloc_Image_reset">
-            <img src="/ressources/dogloverslogo.png" alt="logo"></img>
+    <a href="./MonProfil.php"><img id="bloc_ImageTitre" src="/ressources/logoBis.png" alt="logo"></img></a>
             <div id="oubliage">
+                    
             </div>
         </div>
     </body>
-
     </html>
 <?php
 } else {
