7e53c2a962671ad8225ca772860ac41f885e5ab3
Thomas <leflochtho@eisti.eu>
2020-06-18 18:53:59 +0200

    ajout lien post stack overflow pour cancel le resubmit du formulaire au refresh de la page

--- messagerie/messagerie.php
+++ messagerie/messagerie.php
@@ -23,11 +23,12 @@ if ((isset($_SESSION["login_Type"])) && (intval($_SESSION["login_Type"]) >= 2)) 
     }
   ?>
 
-<script>
+<script> // Merci Stack OverFlow https://stackoverflow.com/questions/6320113/how-to-prevent-form-resubmission-when-page-is-refreshed-f5-ctrlr 
 if ( window.history.replaceState ) {
   window.history.replaceState( null, null, window.location.href );
 }
 </script>
+
 <body>
   <!--Début bloc de présentation-->
   <div id="blocTitre"></div>
