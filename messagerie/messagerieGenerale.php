5d53f66a2c0e928978299d4f11f0ee0bc1e71ab8
Louve Le Bronec <lebroneclo@eisti.eu>
2020-06-22 15:22:16 +0200

    ajout commentaires sur les fichiers de messagerie

--- messagerie/messagerieGenerale.php
+++ messagerie/messagerieGenerale.php
@@ -1,6 +1,7 @@
 <?php
 //on démarre une session
 session_start();
+//on vérifie que l'utilisateur est bien connectée
 if ((isset($_SESSION["login_Type"])) && (intval($_SESSION["login_Type"]) > 0)) { ?>
   <!DOCTYPE html "-//W3C//DTD XHTML 1.0 Strict //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1−strict.dtd">
   <html>
@@ -14,7 +15,7 @@ if ((isset($_SESSION["login_Type"])) && (intval($_SESSION["login_Type"]) > 0)) {
   </head>
 
   <body>
-    <!--Début bloc de présentation-->
+    <!--Début bloc de présentation avec les différents boutons permettant de naviguer entre les pages-->
     <div id="blocTitre"></div>
     <div id="Titre">
       <img src="/ressources/dogloverslogo.png" alt="logoDogLovers">
@@ -23,7 +24,6 @@ if ((isset($_SESSION["login_Type"])) && (intval($_SESSION["login_Type"]) > 0)) {
     <div class="menu">
       <ul>
         <li><a href="../home/accueil.php">Accueil</a></li>
-        <!--<li><a href="./../profil/profil.php?user=<?php /*echo ($_SESSION["user"]);?>">Infos <?php echo ($_SESSION["user"]); */ ?></a></li>-->
         <li><a class="active" href="">Conversations</a></li>
         <li class="deconnexion"><a href="./../login/logout.php">Deconnexion</a></li>
       </ul>
@@ -38,58 +38,85 @@ if ((isset($_SESSION["login_Type"])) && (intval($_SESSION["login_Type"]) > 0)) {
           Retrouvez ici les derniers messages de chaque conversation.
         </div>
         <?php
+        //on regarde si l'utilisateur est abonné
         if ($_SESSION["login_Type"] >= 2) {
+          //on regarde s'il existe un fichier destinataires pour la personne connectée, cad s'il a déjà écrit à quelqu'un
           if (file_exists('destinataires_' . $_SESSION['pseudo'] . '.txt')) {
+            //on récupère le contenu du fichier destinataires_NomDeLaPersonneConnectée.txt
+            //ce fichier contient les noms de toutes les personnes à qui la personne connectée a parlé
             $conversation = file_get_contents('destinataires_' . $_SESSION['pseudo'] . '.txt');
+            /*on sépare selon les |, cad on stock dans un tableau les noms des différentes personnes
+            ainsi par exemple $destinataire[0] sera égale au pseudo de la première personne à qui la personne connectée à parlé.*/
             $destinataire = explode("|", $conversation);
+            //on initialise les variables
+            //i est la variable qu'on incrémente
             $i = 0;
+            //on parcourt tous les noms des destinataires
             while ($i < sizeof($destinataire) - 1) {
+              /*on sépare chaque case du tableau selon les _ car les utilisateurs bloqués sont écrit sous la forme:
+              pseudo_bloque ainsi on peut récupérer juste le pseudo de la personne sans _bloqué*/
               $destinataireBis = explode("_",$destinataire[$i]);
+              //on fait un tableau contenant le pseudo de la personne connectée et celui du destinataire
               $nomFichier = array($_SESSION['pseudo'], $destinataireBis[0]);
               //on les tri par ordre alphabétique
               usort($nomFichier, "strnatcmp");
+              //on explode les deux pseudos (celui de la personne connectée et celui du destinataire)
+              //afin de récupérer juste le pseudo sans le _bloqué s'il est présent
               $tmpNom = explode("_",$nomFichier[0]);
               $tmpNom2 = explode("_",$nomFichier[1]);
+              //on récupère le contenu du fichier nommé pseudo1_pseudo2.txt pseudos 1 et 2
+              //correspondant au pseudo de l'utilisateur et a celui du destinataire triés par ordre alphabétique
+              //ce fichier contient la conversation entre les deux utilisateurs
               $messages = file_get_contents($tmpNom[0] . '_' . $tmpNom2[0] . '.txt');
+              //on sépare selon les lignes pour pouvoir ensuite récupérer seulement le dernier message
               $dernierMessage = explode("\n", $messages);
+              //on regarde si le detinataire est bloqué
+              if(isset($destinataireBis[1]) && $destinataireBis[1] == "bloqué"){
+                //si il est bloqué on écrit pseudoDestinataire est bloqué suivit d'un bouton débloquer pseudoDestinataire
+                ?>
+                <div class="pseudoMess">
+                  <a <?php echo "href='../messagerie/messagerie.php?user=" . $destinataireBis[0] . "'" ?>><?php echo ($destinataireBis[0]) ?></a> est bloqué.
+                </div>
+                <div class="boutonBloquer">
+                  <a <?php echo "href='../messagerie/bloquerUser.php?user=". $destinataireBis[0] ."'"?>>Débloquer <?php echo ($destinataireBis[0] . "<br>"); $_SESSION["BloquerOuDebloquer".$i] = "debloquer"; ?></a>
+                </div>
+                <?php
+                //s'il n'est pas bloqué
+              }else{
+                //on écrit juste le pseudo du destinataire
+                ?>
+                <div class="pseudoMess">
+                  <a <?php echo "href='../messagerie/messagerie.php?user=" . $destinataireBis[0] . "'" ?>><?php echo ($destinataireBis[0]) ?></a>
+                </div>
 
-                          if(isset($destinataireBis[1]) && $destinataireBis[1] == "bloqué"){
-        ?>
-          <div class="pseudoMess">
-          <a <?php echo "href='../messagerie/messagerie.php?user=" . $destinataireBis[0] . "'" ?>><?php echo ($destinataireBis[0]) ?></a> est bloqué.
-        </div>
-        <div class="boutonBloquer">
-<a <?php echo "href='../messagerie/bloquerUser.php?user=". $destinataireBis[0] ."'"?>>Débloquer <?php echo ($destinataireBis[0] . "<br>"); $_SESSION["BloquerOuDebloquer".$i] = "debloquer"; ?></a>
-</div>
-        <?php
-      }else{
-        ?>
-        <div class="pseudoMess">
-          <a <?php echo "href='../messagerie/messagerie.php?user=" . $destinataireBis[0] . "'" ?>><?php echo ($destinataireBis[0]) ?></a>
-        </div>
+                <?php
+              }
+              //ensuite on regarde si il y a un dernier message
+              if (isset($dernierMessage[1])) {
+                //si c'est le cas on le récupère
+                $dernierMessageFlat = explode("§", $dernierMessage[sizeof($dernierMessage) - 2]);
+                //et on l'affiche
+                echo "<div class='mess'>".($dernierMessageFlat[0])."</div>";
+              }
+              //si le destinataire n'est pas bloqué
+              if(!isset($destinataireBis[1])||($destinataireBis[1] != "bloqué")){
+                //on lui donne la possibilté de le bloquer en cliquant sur bouton
+                ?>
+                <div class="boutonBloquer">
+                  <a <?php echo "href='../messagerie/bloquerUser.php?user=". $destinataireBis[0] ."'"?>>Bloquer <?php echo ($destinataireBis[0] . "<br>"); $_SESSION["BloquerOuDebloquer".$i] = "bloquer"; ?></a>
+                </div>
 
-<?php
-    }
-           if (isset($dernierMessage[1])) {
-              //echo sizeof($dernierMessage);
-              //$dernierMessage = array_filter($dernierMessage);
-              $dernierMessageFlat = explode("§", $dernierMessage[sizeof($dernierMessage) - 2]);
-              echo "<div class='mess'>".($dernierMessageFlat[0])."</div>";
-    }
-if(!isset($destinataireBis[1])||($destinataireBis[1] != "bloqué")){
-              ?>
-              <div class="boutonBloquer">
-                <a <?php echo "href='../messagerie/bloquerUser.php?user=". $destinataireBis[0] ."'"?>>Bloquer <?php echo ($destinataireBis[0] . "<br>"); $_SESSION["BloquerOuDebloquer".$i] = "bloquer"; ?></a>
-              </div>
-
-              <?php
-            }
+                <?php
+              }
+              //on passe ensuite au destinataire suivant et on refait pareil
               $i++;
               echo "<div class='clear'></div><br>";
-            }
+            }//fin de la boucle while
+            //si le fichier n'existe pas, donc s'il n'a jamais écrit à personne on le lui signal
           } else {
             echo ("<div id='messPasAbo'> Pour démarrer une conversation avec quelqu'un, rendez-vous sur son profil !</div>");
           }
+          //s'il n'est pas abonné alors on l'informe qu'il doit s'abonner pour voir ses messages
         } else {
           echo "<div id='messPasAbo'>Vous n'êtes pas abonnés... Pour voir vos messages, veuillez-vous abonner !</div>";
         }
@@ -99,8 +126,10 @@ if(!isset($destinataireBis[1])||($destinataireBis[1] != "bloqué")){
   </body>
 
   </html>
-<?php
+  <?php
+  //on libère la variable
   unset($_POST['message']);
+  //s'il n'est pa connecté on renvoi à la page d'erreur
 } else {
   header("Location: /home/erreur403.php");
 }
